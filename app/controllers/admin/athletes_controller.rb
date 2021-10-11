cfc6be64d09cad15ac4804343a97d039c0a5b90e
Joni Vainio-Kaila <jonivainiokaila@gmail.com>
2021-10-11 21:32:35 +0300

    Add default sorting for administrate

--- app/controllers/admin/athletes_controller.rb
+++ app/controllers/admin/athletes_controller.rb
@@ -1,5 +1,9 @@
 module Admin
   class AthletesController < Admin::ApplicationController
+    def default_sorting_attribute
+      :firstname
+    end
+
     def find_resource(param)
       Athlete.friendly.find(param)
     end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,7 @@
 module Admin
   class UsersController < Admin::ApplicationController
+    def default_sorting_attribute
+      :email
+    end
   end
 end
