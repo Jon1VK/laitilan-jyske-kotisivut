9bb4960c0d710899eddb53c24f294cbdafa0906f
Joni Vainio-Kaila <jonivainiokaila@gmail.com>
2021-10-21 23:45:55 +0300

    Create News model and update User model

---
+++ app/models/news.rb
@@ -0,0 +1,39 @@
+# == Schema Information
+#
+# Table name: news
+#
+#  id           :bigint           not null, primary key
+#  published_at :date             not null
+#  slug         :string           not null
+#  title        :string           not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  user_id      :bigint           not null
+#
+# Indexes
+#
+#  index_news_on_slug     (slug) UNIQUE
+#  index_news_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
+class News < ApplicationRecord
+  # Associations
+  belongs_to :user
+  has_rich_text :content
+
+  # Validations
+  validates :title, presence: true, uniqueness: { scope: :published_at }
+  validates :published_at, presence: true
+
+  # Add human friendly slug
+  extend FriendlyId
+  friendly_id :to_slug, use: :slugged
+
+  # Methods
+  def to_slug
+    "#{published_at} #{title}"
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,11 @@
 # Table name: users
 #
 #  id                     :bigint           not null, primary key
+#  admin                  :boolean          default(FALSE)
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
+#  firstname              :string           not null
+#  lastname               :string           not null
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
@@ -17,5 +20,19 @@
 #  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #
 class User < ApplicationRecord
+  default_scope { order(:firstname, :lastname) }
+
   devise :database_authenticatable, :recoverable, :rememberable, :validatable
+
+  # Associations
+  has_many :news
+
+  # Validations
+  validates :firstname, presence: true, uniqueness: { scope: :lastname }
+  validates :lastname, presence: true
+
+  # Methods
+  def fullname
+    "#{firstname} #{lastname}"
+  end
 end
